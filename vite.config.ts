40366e175fb6e914ecd877cb5fd086e4abb1806f
Otinelio <otidumeando@gmail.com>
2026-06-10 00:39:43 +0000

    r

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
-import { nitro } from "nitro/vite"; // <-- Ajout de l'import Nitro
 
 export default defineConfig({
-  vite: { // <-- Ajout de la clé vite pour passer des plugins supplémentaires
-    plugins: [
-      nitro({ preset: "vercel" }) // <-- Force le formatage pour Vercel
-    ]
-  },
+  nitro: { preset: "vercel" },
   tanstackStart: {
     server: { entry: "server" },
   },
